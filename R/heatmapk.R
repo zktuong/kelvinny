dfb4a276466342f765a61ab874aa2dfb49717b8a
Kelvin Tuong <uqztuong@uq.edu.au>
2018-01-19 12:55:40 +1000

    plotHeat

--- R/heatmapk.R
+++ R/heatmapk.R
@@ -14,19 +14,28 @@
 #' @return A wrapper for pheatmap.
 #' @examples
 #' data(iris)
-#' data <- iris[,c(1:4)]
-#' pheatmapping(data)
+#' head(iris)
+#' #   Sepal.Length Sepal.Width Petal.Length Petal.Width Species
+#' #1          5.1         3.5          1.4         0.2  setosa
+#' #2          4.9         3.0          1.4         0.2  setosa
+#' #3          4.7         3.2          1.3         0.2  setosa
+#' #4          4.6         3.1          1.5         0.2  setosa
+#' #5          5.0         3.6          1.4         0.2  setosa
+#' #6          5.4         3.9          1.7         0.4  setosa
+#' df <- iris[,c(1:4)]
+#' plotHeat(df)
 #' @import pheatmap
 #' @import RColorBrewer
 #' @import viridis
 #' @export
-heatmap.k <- function(data , color="RdWhBlu", scale="row", show_colnames=TRUE, show_rownames=TRUE, cluster_rows=TRUE, cluster_cols=TRUE, fontsize=9, n=50, option="A", direction=1) {
-  if(class(data[,1]) != "numeric") {
-  rnames <- data[,1] # assign labels in column 1 to "rnames"
-  mat_data <- data.matrix(data[,2:ncol(data)])    # transform column 2 to last column into a matrix
+plotHeat <- function(dm , color="RdWhBlu", scale="row", show_colnames=TRUE, show_rownames=TRUE, cluster_rows=TRUE, cluster_cols=TRUE, fontsize=9, n=50, option="D", direction=1) {
+  if(dm == null)
+  if(class(dm[,1]) != "numeric") {
+  rnames <- dm[,1] # assign labels in column 1 to "rnames"
+  mat_data <- data.matrix(dm[,2:ncol(m)])    # transform column 2 to last column into a matrix
   rownames(mat_data) <- rnames
   } else {
-  mat_data <- data.matrix(data)
+  mat_data <- data.matrix(dm)
   }
 
   if (color == "RdWhBlu") {
