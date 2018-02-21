ad168237b3319c4c2abf2d033e6a091039fb353b
zktuong <z.tuong@uq.edu.au>
2018-02-22 09:44:59 +1000

    plotheat

--- R/heatmapk.R
+++ R/heatmapk.R
@@ -1,20 +1,23 @@
 #' Plotting a heatmap using pheatmap
 #'
-#' @param data A data frame containing values of interest. Genes in rows, samples across columns.
-#' @param color A string for what colours you want. e.g. c("blue","white,"red")
-#' @param scale see ?pheatmap
-#' @param show_colnames see ?pheatmap
-#' @param show_rownames see ?pheatmap
-#' @param cluster_rows see ?pheatmap
-#' @param cluster_cols see ?pheatmap
-#' @param fontsize see ?pheatmap
+#' @param data A data frame containing values of interest. Genes in rows, samples across columns. if supplied as a matrix with rownames as genes and the 1st column is exprs values, it will still go through.
+#' @param color Default has "BluWhRd", "RdWhBlu", "viridis" or a string of the colours you want. e.g. c("blue","yellow","orange"). 
+#' @param scale Default is "row". Other options include "column" or "none". see ?pheatmap.
+#' @param show_colnames TRUE/FALSE. see ?pheatmap.
+#' @param show_rownames TRUE/FALSE. see ?pheatmap.
+#' @param cluster_rows TRUE/FALSE. see ?pheatmap.
+#' @param cluster_cols TRUE/FALSE. see ?pheatmap.
+#' @param fontsize Default is 9. see ?pheatmap.
+#' @param drop_levels TRUE/FALSE. see ?pheatmap.
+#' @param legend TRUE/FALSE. see ?pheatmap.
+#' @param border_color see ?pheatmap.
 #' @param n see ?viridis
 #' @param alpha see?viridis
 #' @param begin see ?viridis
 #' @param end see ?viridis
 #' @param option see ?viridis
 #' @param direction see ?viridis
-#' @param ... Passes on arguments to pheatmap
+#' @param ... Passes on arguments to pheatmap.
 #' @return A wrapper for pheatmap.
 #' @examples
 #' data(iris)
@@ -54,7 +57,7 @@
 #' @import RColorBrewer
 #' @import viridis
 #' @export
-plotHeat <- function(d , color = "RdWhBlu", scale = "row", n = 50, alpha = 1, begin = 0, end = 1,  option = "D", direction = 1, show_colnames=TRUE, show_rownames=TRUE, cluster_rows=TRUE, cluster_cols=TRUE, fontsize=9, ...) {
+plotHeat <- function(d , color = "RdWhBlu", scale = "row", n = 50, alpha = 1, begin = 0, end = 1,  option = "D", direction = 1, show_colnames=TRUE, show_rownames=TRUE, cluster_rows=TRUE, cluster_cols=TRUE, drop_levels=TRUE, fontsize=9, legend=TRUE, border_color=NA, ...) {
   if(class(d[,1]) != "numeric") {
   rnames <- d[,1] # assign labels in column 1 to "rnames"
   mat_data <- data.matrix(d[,2:ncol(d)])    # transform column 2 to last column into a matrix
@@ -64,24 +67,27 @@ plotHeat <- function(d , color = "RdWhBlu", scale = "row", n = 50, alpha = 1, be
   }
 
   if (color == "RdWhBlu") {
-    col_scale = c(brewer.pal(9, "RdBu"))
-    col_palette <- colorRampPalette(col_scale)(n = n)
+    col_scale = RColorBrewer::brewer.pal(9, "RdBu")
+    col_palette <- grDevices::colorRampPalette(col_scale)(n = n)
+  } else if (color == "BluWhRd"){
+    col_scale = rev(RColorBrewer::brewer.pal(9, "RdBu"))
+    col_palette <- grDevices::colorRampPalette(col_scale)(n = n)
   } else if (color == "viridis") {
-    col_scale = viridis(n, alpha = alpha, begin = begin, end = end, direction = direction, option = option)
+    col_scale = viridisLite::viridis(n, alpha = alpha, begin = begin, end = end, direction = direction, option = option)
     col_palette = col_scale
     } else if (color == color) {
     col_scale = color
-    col_palette <- colorRampPalette(col_scale)(n = n)
+    col_palette <- grDevices::colorRampPalette(col_scale)(n = n)
   }
-  p <- pheatmap(
+  p <- pheatmap::pheatmap(
     mat               = mat_data,
     color             = col_palette,
-    border_color      = NA,
+    border_color      = border_color,
     scale = scale,
-    legend = TRUE,
-    show_colnames     = TRUE,
-    show_rownames     = TRUE,
-    drop_levels       = TRUE,
+    legend = legend,
+    show_colnames     = show_colnames,
+    show_rownames     = show_rownames,
+    drop_levels       = drop_levels,
     fontsize          = fontsize,
     cluster_rows = cluster_rows,
     cluster_cols = cluster_cols,
