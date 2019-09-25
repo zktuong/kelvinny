74c28e8e59e6b5d425968b31a4e24c63f4b90d97
Kelvin <kt16@sanger.ac.uk>
2019-09-25 16:15:32 +0100

    Update trainScSimilarity.R

--- R/trainScSimilarity.R
+++ R/trainScSimilarity.R
@@ -69,12 +69,12 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
         Zero_col <- which(colSums(train_dat) == 0)
         duplicated_col <- which(duplicated(colnames(train_dat)) == TRUE)
         if (length(c(Zero_col, duplicated_col)) != 0) {
-            print(paste0("removing ", length(c(Zero_col, duplicated_col)), 
+            print(paste0("Removing ", length(c(Zero_col, duplicated_col)), 
                 " genes with no variance"))
             train_dat <- train_dat[, -c(Zero_col, duplicated_col)]
         }
         if (standardize == TRUE) {
-            print("standardizing prediction/target dataset")
+            print("Standardizing training dataset")
             train_dat <- standardizeSparse(train_dat)
         }
         
@@ -85,12 +85,12 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
         }
         
         if (multinomial == FALSE) {
-            print("family = binomial")
+            print("Training modeil with family = binomial")
             for (label in labels) {
                 print(paste0("Training model for ", label))
                 celltype = factor(train_cell_type == label)
                 
-                fit[[L]] = tryCatch(glmnet::cv.glmnet(train_dat, celltype, 
+                fit[[label]] = tryCatch(glmnet::cv.glmnet(train_dat, celltype, 
                   offset = getPopulationOffset(celltype), family = "binomial", 
                   alpha = a, nfolds = nfolds, type.measure = "class", parallel = nParallel, 
                   ...), error = function(e) {
@@ -104,7 +104,7 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
                   })
                 })
             }
-            print("extracting best gene features...")
+            print("Extracting best gene features...")
             for (i in 1:length(fit)) {
                 if (l.min) {
                   print("Choosing the best model but with the caveat that may be too complex, may be slightly overfitted")
@@ -121,7 +121,7 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
             }
             return(fit)
         } else {
-            print("family = mulinomial")
+            print("Training modeil with family = mulinomial")
             fit <- tryCatch(glmnet::cv.glmnet(train_dat, train_cell_type, 
                 family = "multinomial", alpha = a, nfolds = nfolds, type.measure = "class", 
                 parallel = nParallel, ...), error = function(e) {
@@ -133,7 +133,7 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
                   return(NULL)
                 })
             })
-            print("extracting best gene features...")
+            print("Extracting best gene features...")
             for (i in 1:length(fit)) {
                 if (l.min) {
                   fit_out <- as.matrix(coef(fit, s = fit$lambda.min)[[i]])
@@ -184,7 +184,7 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
         train_dat <- t(train_dat)
         
         if (standardize == TRUE) {
-            print("standardizing training data")
+            print("Standardizing training dataset")
             train_dat <- standardizeSparse(train_dat)
         }
         
@@ -194,11 +194,11 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
             labels <- levels(train_cell_type)
         }
         if (multinomial == FALSE) {
-            print("family = binomial")
+            print("Training modeil with family = binomial")
             for (label in labels) {
                 print(paste0("Training model for ", label))
                 celltype = factor(train_cell_type == label)
-                fit[[L]] = tryCatch(glmnet::cv.glmnet(train_dat, celltype, 
+                fit[[label]] = tryCatch(glmnet::cv.glmnet(train_dat, celltype, 
                   family = "binomial", alpha = a, nfolds = nfolds, type.measure = "class", 
                   parallel = nParallel, ...), error = function(e) {
                   tryCatch(glmnet::cv.glmnet(train_dat, celltype, family = "binomial", 
@@ -211,7 +211,7 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
                   })
                 })
             }
-            print("extracting best gene features...")
+            print("Extracting best gene features...")
             for (i in 1:length(fit)) {
                 if (l.min) {
                   print("Choosing the best model but with the caveat that may be too complex, may be slightly overfitted")
@@ -228,7 +228,7 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
             }
             return(fit)
         } else {
-            print("family = mulinomial")
+            print("Training modeil with family = mulinomial")
             fit <- tryCatch(glmnet::cv.glmnet(train_dat, train_cell_type, 
                 family = "multinomial", alpha = a, nfolds = nfolds, type.measure = "class", 
                 parallel = nParallel, ...), error = function(e) {
@@ -240,7 +240,7 @@ trainScSimilarity <- function(train_data, train_cell_type, train_genes = NULL,
                   return(NULL)
                 })
             })
-            print("extracting best gene features...")
+            print("Extracting best gene features...")
             for (i in 1:length(fit)) {
                 if (l.min) {
                   fit_out <- as.matrix(coef(fit, s = fit$lambda.min)[[i]])
