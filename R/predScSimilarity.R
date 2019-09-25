e49290e31eaebc186f7ad256674b9423fe8ec047
Kelvin <kt16@sanger.ac.uk>
2019-09-25 21:47:38 +0100

    Update predScSimilarity.R

--- R/predScSimilarity.R
+++ R/predScSimilarity.R
@@ -1,10 +1,10 @@
 #' Predicts scRNA-seq data from trained multinomial cv.glmnet model
 #' 
 #' @param model trained model.
-#' @param test seurat object, SummarizedExperiment object or expression matrix for prediction
+#' @param test Seurat object, SummarizedExperiment object or expression matrix for prediction
 #' @param standardize a logical value specifying whether or not to standardize the test matrix
-#' @param lambda.1se logical value. FALSE will use lambda.min for prediction
-#' @param ... pass to predict. see ?predict.glmnet
+#' @param l.min logical. FALSE will use lambda.1se for prediction
+#' @param ... pass to predict function see ?predict.glmnet
 #' @return Generates prediction.
 #' @examples
 #' pred <- predScSimilarity(model, test.sce)
@@ -13,53 +13,79 @@
 #' @export
 #'      
             
-predScSimilarity <- function(model, test, standardize = TRUE, lambda.1se = TRUE, ...) {
+predScSimilarity <- function(model, test, standardize = TRUE, l.min = FALSE, ...) {
 
     preds <- list()
-    model.genes <- list()
-    trained.class <- names(model)
-
-    standardizing <- function(X) {
-        X <- X - mean(X)
-        X <- X/sd(X)
-        return(X)
+    model.genes <- list()    
+    
+    standardizeSparse <- function(A) {
+        A@x <- A@x/rep.int(colSums(A), diff(A@p))
+        return(A)
     }
 
     if(class(test) == "SummarizedExperiment"){
-        newx <- t(as.matrix(SummarizedExperiment::assay(test)))
+        newx <- SummarizedExperiment::assay(test)
     } else if (class(test) == "Seurat"){
-        newx <- tryCatch(
-        t(as.matrix(test@data)), error = function(e) {
-            tryCatch(
-                t(as.matrix(GetAssayData(object = test))), error = function(e) {
+        newx <- tryCatch(test@data, error = function(e) {
+            tryCatch(GetAssayData(object = test), error = function(e) {
                 warning(paste0("are you sure this is a seurat v3 object?"))
                 return(NULL)
-    })})} else {
-        newx <- t(as.matrix(test))
+    		})
+        })    
+	} else {
+        newx <- test
     }
 
-    if (standardize == TRUE) {
-        print("standardizing test data")
-        newx <- t(apply(newx, 1, standardizing))
-        }
-    
-    if (lambda.1se == TRUE) {
-        for(class in trained.class){
-        print(paste0("Predicting probabilities for ", class))
-        model.genes[[class]] <- match(rownames(model[[class]]$glmnet.fit$beta), colnames(newx))
+	if (class(newx) == "matrix") {
+		newx <- Matrix::Matrix(newx, sparse = TRUE)
+	}
+	print("Transposing test matrix")
+	newx <- t(newx)    
 
-        preds[[class]] = predict(model[[class]], newx = newx[,model.genes[[class]]], s = model[[class]]$lambda.1se, newoffset = rep(0, nrow(newx)), ...)
-        colnames(preds[[class]]) <- class        
-        } 
+	if (standardize == TRUE) {
+		print("Standardizing training dataset")
+		newx <- standardizeSparse(newx)
+	}
+
+	if(class(model) == "list"){
+		trained.class <- names(model)
+		trained.class <- factor(trained.class, levels = trained.class)
     } else {
-        for(class in trained.class){
-        print(paste0("Predicting probabilities for ", class))
-        model.genes[[class]] <- match(rownames(model[[class]]$glmnet.fit$beta), colnames(newx))
-
-        preds[[class]] = predict(model[[class]], newx = newx[,model.genes[[class]]], s = model[[class]]$lambda.min, newoffset = rep(0, nrow(newx)), ...)
-        colnames(preds[[class]]) <- class           
-        }
+    	trained.class <- names(model$glmnet.fit$beta)
+    	trained.class <- factor(trained.class, levels = trained.class)
     }
+
+	if(class(model) == "list"){
+    	if (l.min == FALSE) {
+        	for(class in trained.class){
+        	print(paste0("Predicting probabilities for ", class))
+        	model.genes[[class]] <- match(rownames(model[[class]]$glmnet.fit$beta), colnames(newx))
+        	
+        	preds[[class]] <- predict(model[[class]], newx = newx[,model.genes[[class]]], s = model[[class]]$lambda.1se, newoffset = rep(0, nrow(newx)), ...)
+        	colnames(preds[[class]]) <- class        
+        	} 
+    	} else {
+        	for(class in trained.class){
+        	print(paste0("Predicting probabilities for ", class))
+        	model.genes[[class]] <- match(rownames(model[[class]]$glmnet.fit$beta), colnames(newx))
+	
+        	preds[[class]] = predict(model[[class]], newx = newx[,model.genes[[class]]], s = model[[class]]$lambda.min, newoffset = rep(0, nrow(newx)), ...)
+        	colnames(preds[[class]]) <- class           
+        	}
+        }
+    } else {
+    	print("Predicting probabilities")
+    	if (l.min == FALSE) {    		
+    		model.genes <- match(rownames(model$glmnet.fit$beta[[1]]), colnames(newx))
+    		preds <- predict(model, newx = newx[,model.genes], s = model$lambda.1se, ...)
+    	} else {
+    		model.genes <- match(rownames(model$glmnet.fit$beta[[1]]), colnames(newx))
+    		preds <- predict(model, newx = newx[,model.genes], s = model$lambda.min, ...)
+    	}
+
+	}
+    preds <- as.data.frame(preds)
+    colnames(preds) <- gsub("[.]1$", "", colnames(preds))
     return(preds)
 }
 
