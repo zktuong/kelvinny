457403b137a55d4a42bda7f55fb9e2aa87180c10
Kelvin <kt16@sanger.ac.uk>
2019-09-26 11:31:03 +0100

    Update similarity_bootstrap.R

--- R/similarity_bootstrap.R
+++ R/similarity_bootstrap.R
@@ -4,13 +4,9 @@
 #' @param trainingCellType The cell types/clusters in the training data set
 #' @param testingSet Seurat object, SummarizedExperiment object or expression matrix for testing
 #' @param nboots integet specifying number of bootstrap runs to run
-#' @param trainingGenes Genes to use for training. If not provided, it will try to pick from all genes in the training dataset.
-#' @param scale a logical value specifying whether or not to standardize the train matrix
-#' @param nfold.cv integer specifying bin for cross validation. Use all samples if doing LOOCV.
-#' @param a.parameter tunable regularization parameter. 0 = ridge (L2), 1 = LASSO (L1), in between = Elastic-net
-#' @param lambda.min logical. Choose between lambda.min or lambda.1se
-#' @param family.multinomial logical. Choose between family = 'binomial' or 'multinomial'.
+#' @param simplify logical. collapse the predictions to a single data frame
 #' @param nCores integer specifying number of cores for parallelization.
+#' @param verbose logical. if TRUE, print the training/prediction steps. Otherwise, minimal messages will be returned on the screen
 #' @param ... other functions pass to glmnet and predict. see ?glmnet and ?predict.glmnet
 #' @return Generates prediction.
 #' @examples
@@ -23,23 +19,55 @@
 #' @export
 #' 
 
-similarity_bootstrap <- function(trainingSet, trainingCellType, testingSet, nboots = 50, simplify = FALSE, trainingGenes = NULL,
-    scale = TRUE, nfold.cv = 10, a.parameter = 0.9, lambda.min = FALSE, family.multinomial = TRUE, 
-    nCores = parallel::detectCores(), ...){
+similarity_bootstrap <- function(trainingSet, trainingCellType, testingSet, nboots = 50, simplify = FALSE, nCores = parallel::detectCores(), verbose = FALSE, ...){
     
-    prediction <- list()
-    for (i in 1:nboots) {
-        print(paste0("Running job #", i))
-        fit <- trainScSimilarity(train_data = trainingSet, train_cell_type = trainingCellType, test_data = testingSet, train_genes = trainingGenes, standardize = scale, nfolds = nfold.cv, a = a.parameter, l.min = lambda.min, multinomial = family.multinomial, nParallel = nCores, ...)
-        prediction[[i]] <- predScSimilarity(model = fit, test = testingSet, standardize = scale, l.min = lambda.min, ...)
-        print(paste0("Finished job #", i))        
-    }
+    sdList <- function(pred.list) {
+    n <- length(pred.list); 
+    rc <- dim(pred.list[[1]]); 
+    ar <- array(unlist(pred.list), c(rc, n), list(rownames(pred.list[[1]]), colnames(pred.list[[1]]))); 
+    sdf <- apply(ar, c(1, 2), sd)
+    return(sdf)}
+
+    require(foreach)
+    require(doMC)
+    doMC::registerDoMC(cores = nCores)
+
+    if(verbose){
+        prediction <- foreach(i = 1:nboots) %dopar% {
+            prediction <- foreach(i = 1:nboots) %dopar% {
+            cat(paste0("Training bootstrap #", i))
+            fit_model <- trainScSimilarity(train_data = trainingSet, train_cell_type = trainingCellType, test_data = testingSet, ...)
+            cat(paste0("Predicting bootstrap #", i))
+            pred <- predScSimilarity(model = fit_model, test = testingSet, ...)
+            cat(green(paste0("Finished bootstrap #", i)))
+            return(pred)
+            }
+        } else {
+            print(paste0("Training bootstrap #", i))
+            fit_model <- invisible(trainScSimilarity(train_data = trainingSet, train_cell_type = trainingCellType, test_data = testingSet, ...))
+            cat(paste0("Predicting bootstrap #", i))
+            pred <- invisible(predScSimilarity(model = fit_model, test = testingSet, ...))
+            cat(paste0("Finished bootstrap #", i))
+            return(pred)
+            } 
+        }
     
-    if(simplify){
-        print("Averaging predictions")
-        prediction <- Reduce("+", prediction) / length(prediction)
-        return(prediction)
-    } else {            
+    if(length(prediction) > 1) {
+        prediction_summary <- list()
+        if(simplify) {
+            cat("Averaging predictions")
+            prediction_summary[["prediction"]] <- Reduce("+", prediction) / length(prediction)
+            cat("Obtaining standard deviations")
+            prediction_summary[["SD"]] <- sdList(prediction)
+            return(prediction_summary)
+        } else {
+            cat("Returning all predictions")
+            return(prediction)
+        }
+    } else {
+        if(simplify) {
+            warning("Cannot average predictions nor return SDs because only 1 run of training/prediction performed")
+        }
         return(prediction)
     }
 }
